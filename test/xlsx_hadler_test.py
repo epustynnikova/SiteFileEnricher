5ff6c982f9da576ffea6422adfbbed3da73bdd6f
Elena Pustynnikova <e.pustynnikova@bestdoctor.ru>
2025-08-05 15:41:01 +0300

    change input data for xlsx write test

--- test/xlsx_hadler_test.py
+++ test/xlsx_hadler_test.py
@@ -11,7 +11,7 @@ class TestXLSXFileHandler(unittest.TestCase):
     def test_read(self):
         # given:
         path = 'sources/xlsx/short_example.xlsx'
-        output_file_path= 'sources/xlsx'
+        output_file_path = 'sources/xlsx'
         output_file_name = 'short_example.xlsx'
         handler = XLXSFileHandler(path, [], output_file_path, output_file_name)
         test_data = {
@@ -38,7 +38,7 @@ class TestXLSXFileHandler(unittest.TestCase):
     def test_read_ktru(self):
         # given:
         path = 'sources/xlsx/testold.xlsx'
-        output_file_path= 'sources/xlsx'
+        output_file_path = 'sources/xlsx'
         output_file_name = 'testold.xlsx'
         handler = XLXSFileHandler(path, ['field_a'], output_file_path, output_file_name)
 
@@ -46,39 +46,72 @@ class TestXLSXFileHandler(unittest.TestCase):
         result = handler.read()
 
         # then:
-        print(result['https://zakupki.gov.ru/epz/contract/contractCard/common-info.html?reestrNumber=2320300308025000015'])
+        print(result[
+                  'https://zakupki.gov.ru/epz/contract/contractCard/common-info.html?reestrNumber=2320300308025000015'])
 
     def test_write(self):
         # given:
-        path = 'sources/xlsx/short_example_1.xlsx'
-        shutil.copy('sources/xlsx/short_example.xlsx', path)
-        output_file_path= 'sources/xlsx'
-        output_file_name = 'short_example_1.xlsx'
+        path = 'sources/xlsx/short_example.xlsx'
+        output_file_path = 'sources/xlsx'
+        output_file_name = 'short_example.xlsx'
         handler = XLXSFileHandler(path, ['field_a', 'field_b'], output_file_path, output_file_name)
         additional_elements = [
-            OutputElement(1, "", [FileColData(1, 'field_a', 'a_1'), FileColData(2, 'field_b', 'b_1')]),
-            OutputElement(2, "", [FileColData(1, 'field_a', 'a_2'), FileColData(2, 'field_b', 'b_2')]),
-            OutputElement(52, "", [FileColData(1, 'field_a', 'a_52')]),
-            OutputElement(53, "", [FileColData(1, 'field_a', 'a_53')]),
-            OutputElement(54, "", [FileColData(1, 'field_b', 'a_54')])
+            OutputElement(0,
+                          "https://zakupki.gov.ru/epz/contractfz223/card/contract-info.html?id=21013248",
+                          [FileColData(1, 'field_a', 'a_1'), FileColData(2, 'field_b', 'b_1')]),
+            OutputElement(1,
+                          "https://zakupki.gov.ru/epz/contract/contractCard/common-info.html?reestrNumber=2760602879024000424",
+                          [FileColData(1, 'field_a', 'a_2'), FileColData(2, 'field_b', 'b_2')]),
+            OutputElement(2,
+                          "https://zakupki.gov.ru/epz/contract/contractCard/common-info.html?reestrNumber=2760602879024000397",
+                          [FileColData(1, 'field_a', 'a_2'), FileColData(2, 'field_b', 'b_2')]),
+            OutputElement(52,
+                          "https://zakupki.gov.ru/epz/contract/contractCard/common-info.html?reestrNumber=1781308525024000662",
+                          [FileColData(1, 'field_a', 'a_52')]),
+            OutputElement(53,
+                          "https://zakupki.gov.ru/epz/contract/contractCard/common-info.html?reestrNumber=1781308525024000662",
+                          [FileColData(1, 'field_b', 'a_53')]),
+            OutputElement(54,
+                          "'https://zakupki.gov.ru/epz/contract/contractCard/common-info.html?reestrNumber=2032311510424000040'",
+                          [FileColData(1, 'field_a', 'a_54')])
         ]
         data = dict(
             [(add_el.index_in_input_file,
               dict([(cdt.name, cdt.value) for cdt in add_el.new_col_datas]))
              for add_el in additional_elements])
+        result_path = os.path.join(output_file_path, '0_' + output_file_name)
 
         # when:
-        handler.write(additional_elements)
+        handler.write(
+            additional_elements[:1],
+            link="https://zakupki.gov.ru/epz/contractfz223/card/contract-info.html?id=21013248"
+        )
+        handler.write(
+            additional_elements[1:2],
+            link="https://zakupki.gov.ru/epz/contract/contractCard/common-info.html?reestrNumber=2760602879024000424"
+        )
+        handler.write(
+            additional_elements[2:3],
+            link="https://zakupki.gov.ru/epz/contract/contractCard/common-info.html?reestrNumber=2760602879024000397"
+        )
+        handler.write(
+            additional_elements[3:4],
+            link="https://zakupki.gov.ru/epz/contractfz223/card/contract-info.html?id=20879249"
+        )
+        handler.write(
+            additional_elements[4:],
+            link="https://zakupki.gov.ru/epz/contract/contractCard/common-info.html?reestrNumber=1781308525024000662"
+        )
 
         # then:
-        result = pd.read_excel(path)
+        result = pd.read_excel(result_path)
         for idx, row in result.iterrows():
             if idx in data:
                 col_datas = data[idx]
                 for col_name in col_datas:
                     self.assertEqual(row[col_name], col_datas[col_name])
 
-        os.remove(path)
+        os.remove(result_path)
 
 
 if __name__ == '__main__':
